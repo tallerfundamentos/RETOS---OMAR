c6760931ba8b6f2ad45c4aa67d3f2c3ed2cd6b0b
Jesus Omar <158229182+tallerfundamentos@users.noreply.github.com>
2024-01-31 07:13:43 +0000

    Pending changes exported from your codespace

---
+++ Primer Programa/Hola Mundo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int x;
+int y;
+
+int main() {
+
+    cout << "Ingresar x: " << endl;
+    cin >> x;
+    cout << "Ingresar y: " << endl;
+    cin >> y;
+    
+    cout << "Suma Variables: " << x + y << endl; 
+
+    return 0;
+}
